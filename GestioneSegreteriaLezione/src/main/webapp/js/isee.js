7ebe238e0cb33017495a26076c90a8195834d11a
Kristian <kristian.reale@gmail.com>
2020-11-13 09:34:49 +0100

    Start js

---
+++ GestioneSegreteriaLezione/src/main/webapp/js/isee.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+// Chiedere il numero dei componenti del nucleo familiare
+
+var numeroComponenti;  //undefined
+
+numeroComponenti = prompt("Inserisci il numero dei componenti.");
+
+alert(numeroComponenti);
